26f50d92e858941be6790ef31a73a8443bba85a5
NacixWosinska <95026929+NacixWosinska@users.noreply.github.com>
2022-05-16 21:02:48 +0200

    another try

--- android/AppTeczkaAndroid/app/src/main/java/edu/ib/appteczkaandroid/MainActivity.java
+++ android/AppTeczkaAndroid/app/src/main/java/edu/ib/appteczkaandroid/MainActivity.java
@@ -1,21 +1,12 @@
 package edu.ib.appteczkaandroid;
 
-import android.content.IntentFilter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-
-import android.widget.Button;
-import android.widget.TextView;
-import java.io.IOException;
-
-import okhttp3.ResponseBody;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,16 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
-
 
     public void btnGo(View view) {
-        Intent intent = new Intent(this, PlannerController.class);
+//        Intent intent = new Intent(this, PlannerController.class);
+//        startActivity(intent);
+        Intent intent = new Intent(this,InteractionsActivity.class);
         startActivity(intent);
-        finish();
-       // Intent intent = new Intent(this,InteractionsActivity.class);
-        //startActivity(intent);
     }
-
 }
