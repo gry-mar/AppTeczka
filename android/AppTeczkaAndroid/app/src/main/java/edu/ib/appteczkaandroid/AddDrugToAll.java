2fc7afccee5486f82fea679eaa50b2659fe98ee8
Martyna Fojcik <254681@student.pwr.edu.pl>
2022-06-02 19:33:12 +0200

    Add and remove from firebase

--- android/AppTeczkaAndroid/app/src/main/java/edu/ib/appteczkaandroid/AddDrugToAll.java
+++ android/AppTeczkaAndroid/app/src/main/java/edu/ib/appteczkaandroid/AddDrugToAll.java
@@ -1,35 +1,49 @@
 package edu.ib.appteczkaandroid;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TableLayout;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
+import com.google.protobuf.StringValue;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 public class AddDrugToAll extends AppCompatActivity {
 
     final Calendar calendar = Calendar.getInstance();
     private EditText etName, etDate;
 
-    private int yearInt, monthInt, dayInt;
-    private String name;
+    private int yearInt, monthInt, dayInt, drugId;
+    private String drugName, drugDate, year, month, day;
+
+    private DrugInAll drugInfo;
+
+    private Button btnAddDrug;
+    private ImageButton btnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_drug_to_all);
 
-        etName = findViewById(R.id.etDrugName);
-
-        etDate = (EditText) findViewById(R.id.etDrugDateExp);
 
         DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
             @Override
@@ -42,32 +56,69 @@ public class AddDrugToAll extends AppCompatActivity {
             }
         };
 
+        etName = findViewById(R.id.etDrugName);
+        etDate = findViewById(R.id.etDrugDateExp);
+
         etDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 new DatePickerDialog(AddDrugToAll.this, date, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show();
             }
         });
+
+        btnAddDrug = findViewById(R.id.btnAddDrugFinal);
+
+        btnAddDrug.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                drugName = etName.getText().toString();
+                drugDate = etDate.getText().toString();
+
+                yearInt = calendar.get(Calendar.YEAR);
+                monthInt = calendar.get(Calendar.MONTH)+1;
+                dayInt = calendar.get(Calendar.DAY_OF_MONTH);
+
+                year = String.valueOf(yearInt);
+                month = String.valueOf(monthInt);
+                day = String.valueOf(dayInt);
+
+                if(etDate.getText().equals("") || etName.getText() ==(null)){
+                    Toast.makeText(AddDrugToAll.this, "Aby dodać lek należy wypełnić wszystkie pola.", Toast.LENGTH_SHORT).show();
+                }else{
+                    FirebaseFirestore db = FirebaseFirestore.getInstance();
+                    Map<String, Object> data = new HashMap<>();
+                    data.put(drugName, drugDate);
+
+                    db.collection("useremail@gmail.com").document("lekiWszystkie")
+                            .set(data, SetOptions.merge()).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if(task.isSuccessful())
+                                Toast.makeText(AddDrugToAll.this,"Lek został poprawnie dodany",Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            }
+        });
+
+        btnBack = findViewById(R.id.btnBack);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), AllDrugs.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 
     private void updateLabel(EditText et){
         String myFormat="yyyy-MM-dd";
         SimpleDateFormat dateFormat=new SimpleDateFormat(myFormat, Locale.US);
         et.setText(dateFormat.format(calendar.getTime()));
-    }
-
-    public void btnBackOnClick(View view) {
-    }
-
-    public void btnAddDrugFinalOnClick(View view) {
-        name = etName.getText().toString().trim();
-
-        yearInt = calendar.get(Calendar.YEAR);
-        monthInt = calendar.get(Calendar.MONTH)+1;
-        dayInt = calendar.get(Calendar.DAY_OF_MONTH);
-
-        //TUTAJ MUSI BY DODAWANIE DO BAZY, NO A PRZY ALLDRUGS TO ODCZYT Z BAZY
-
-
     }
 }
--- android/AppTeczkaAndroid/app/src/main/java/edu/ib/appteczkaandroid/AllDrugs.java
+++ android/AppTeczkaAndroid/app/src/main/java/edu/ib/appteczkaandroid/AllDrugs.java
@@ -1,172 +1,189 @@
 package edu.ib.appteczkaandroid;
 
-import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ListView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firestore.v1.WriteResult;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 import java.util.Map;
 
 public class AllDrugs extends AppCompatActivity {
 
     private ArrayList<DrugInAll> drugsList;
 
-    // tu powinna być klasa przechowująca obiekty {name, date}
-
-    private String lek1, lek2, data1, data2, lek3, data3;
+    private String drugName, drugExpDate;
     private int state, rowId, rowIdOld;
     private String chosenName;
     private String chosenDate;
-    //public AllDrugs(ArrayList<DrugInAll> drugsList, Context context) {
-    //    this.drugsList = drugsList;
-    //    this.context = context;
-    //}
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_drugs);
-        lek1 = "Lek1";
-        lek2 = "Lek2";
-        data1 = "06-06-2026";
-        data2 = "08-08-2028";
-        lek3 = "Lek3";
-        data3 = "04-04-2024";
+
         state = 0;
 
         Button btnRemoveDrug = findViewById(R.id.btnRemoveDrug);
 
-        //TU TRZEBA ZMIENIć NA DATE JAK JUŻ BĘDZIE BAZA i w ogole ustawić tak,
-        // że cała ta lista ściągana z bazy będzie słownikiem, a po kliknięciu adda to sie doda nowa wartosc,
-        // czyli trzeba będzie to wrzucić dodawanie do innej metody
         FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-        //po dodaniu czegoś do dawkowania
-
         Map<String,String> drug = new HashMap<String,String>();
-        drug.put(lek1, data1);
-        drug.put(lek2, data2);
-        drug.put(lek3, data3);
 
-
-        // TEN LEPSZY SPOSOB
         db.collection("useremail@gmail.com").document("lekiWszystkie")
-                .set(drug).addOnCompleteListener(new OnCompleteListener<Void>() {
+                .get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
             @Override
-            public void onComplete(@NonNull Task<Void> task) {
-                if(task.isSuccessful())
-                    Toast.makeText(AllDrugs.this,"sposob lepszy v1",Toast.LENGTH_SHORT).show();
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    // Document found in the offline cache
+                    DocumentSnapshot document = task.getResult();
+
+                    ArrayList<Map<String, Object>> drugs = new ArrayList<>();
+                    drugs.add(task.getResult().getData());
+                    System.out.println(drugs);
+
+                    String[] splitdrugs = drugs.toString().split(",");
+                    for(int i=0; i<task.getResult().getData().size(); i++){
+                        splitdrugs[i] = splitdrugs[i].replace("{", "");
+                        splitdrugs[i] = splitdrugs[i].replace("}", "");
+                        splitdrugs[i] = splitdrugs[i].replace("[", "");
+                        splitdrugs[i] = splitdrugs[i].replace("]", "");
+                        String[] splitOneDrug = splitdrugs[i].split("=");
+                        drug.put(String.valueOf(splitOneDrug[0]), String.valueOf(splitOneDrug[1]));
+
+                    }
+
+                    TableLayout table = (TableLayout)AllDrugs.this.findViewById(R.id.tableAllDrugs);
+                    int i = 0;
+                    for (Map.Entry<String, String> drugEntry : drug.entrySet() ) {
+                        String key = drugEntry.getKey();
+                        String value = drugEntry.getValue();
+                        //drugsList.add(d, drugEntry.getValue());
+                        final TableRow row = new TableRow(AllDrugs.this);
+
+                        TextView tv1 = new TextView(AllDrugs.this);
+                        tv1.setText(key);
+                        tv1.setTextColor(Color.parseColor("#3e3e3e"));
+                        tv1.setGravity(Gravity.CENTER);
+                        tv1.setPadding(0,10,0,10);
+                        row.addView(tv1);
+
+                        TextView tv2 = new TextView(AllDrugs.this);
+                        tv2.setText(value);
+                        tv2.setTextColor(Color.parseColor("#3e3e3e"));
+                        tv2.setGravity(Gravity.CENTER);
+                        tv2.setPadding(0,10,0,10);
+                        row.addView(tv2);
+
+                        row.setId(i);
+                        row.setClickable(true);
+                        row.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                if(state == 0){
+                                    row.setBackgroundColor(Color.parseColor("#adadad"));
+                                    state = 1;
+                                    System.out.println("3 " + row.getId());
+                                    rowId = row.getId();
+                                    rowIdOld = rowId;
+
+                                    TextView name = (TextView) row.getChildAt(0);
+                                    TextView date= (TextView) row.getChildAt(1);
+                                    chosenName = name.getText().toString();
+                                    chosenDate = date.getText().toString();
+                                    System.out.println( chosenName);
+                                    System.out.println(chosenDate);
+
+                                }else if(state == 1){
+                                    row.setBackgroundColor(Color.TRANSPARENT);
+                                    state = 0;
+                                }
+                            }
+                        });
+
+                        table.addView(row);
+                        i++;
+                    }
+                    Button startDosage = findViewById(R.id.btnStartDrug);
+                    startDosage.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            if((chosenDate != null) || (chosenName != null)) {
+                                Intent intent = new Intent(AllDrugs.this, DosageActivity.class);
+                                Bundle bundle = new Bundle();
+                                bundle.putString("name",  chosenName);
+                                bundle.putString("date", chosenDate);
+                                bundle.putInt("id", rowId);
+                                intent.putExtras(bundle);
+                                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                startActivity(intent);
+                                System.out.println(String.valueOf(intent));
+                                finish();
+                            }else{
+                                Toast.makeText(AllDrugs.this,"Aby przejść do dawkowania, " +
+                                        "najpierw zaznacz lek.",Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                    table.requestLayout();
+
+
+                } else {
+                    Toast.makeText(AllDrugs.this,"Wystąpił błąd. Proszę spróbować ponownie.",Toast.LENGTH_SHORT).show();
+                }
             }
         });
-
-
-        TableLayout table = (TableLayout)AllDrugs.this.findViewById(R.id.tableAllDrugs);
-        int i = 0;
-        for (Map.Entry<String, String> drugEntry : drug.entrySet() ) {
-            String key = drugEntry.getKey();
-            String value = drugEntry.getValue();
-            //drugsList.add(d, drugEntry.getValue());
-            final TableRow row = new TableRow(this);
-
-            TextView tv1 = new TextView(this);
-            tv1.setText(key);
-            tv1.setTextColor(Color.parseColor("#3e3e3e"));
-            tv1.setGravity(Gravity.CENTER);
-            tv1.setPadding(0,10,0,10);
-            row.addView(tv1);
-
-            TextView tv2 = new TextView(this);
-            tv2.setText(value);
-            tv2.setTextColor(Color.parseColor("#3e3e3e"));
-            tv2.setGravity(Gravity.CENTER);
-            tv2.setPadding(0,10,0,10);
-            row.addView(tv2);
-
-            row.setId(i);
-            row.setClickable(true);
-            row.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    if(state == 0){
-                        row.setBackgroundColor(Color.parseColor("#adadad"));
-                        state = 1;
-                        System.out.println("3 " + row.getId());
-                        rowId = row.getId();
-                        rowIdOld = rowId;
-
-                        TextView name = (TextView) row.getChildAt(0);
-                        TextView date= (TextView) row.getChildAt(1);
-                        chosenName = name.getText().toString();
-                        chosenDate = date.getText().toString();
-                        System.out.println( chosenName);
-                        System.out.println(chosenDate);
-
-                    }else if(state == 1){
-                        row.setBackgroundColor(Color.TRANSPARENT);
-                        state = 0;
-                    }
-                }
-            });
-
-            table.addView(row);
-            i++;
-        }
-        Button startDosage = findViewById(R.id.btnStartDrug);
-        startDosage.setOnClickListener(new View.OnClickListener() {
+        btnRemoveDrug.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if((chosenDate != null) || (chosenName != null)) {
-                    Intent intent = new Intent(AllDrugs.this, DosageActivity.class);
-                    Bundle bundle = new Bundle();
-                    bundle.putString("name",  chosenName);
-                    bundle.putString("date", chosenDate);
-                    bundle.putInt("id", rowId);
-                    intent.putExtras(bundle);
-                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                    startActivity(intent);
-                    System.out.println(String.valueOf(intent));
-                    finish();
-                }else{
-                    Toast.makeText(AllDrugs.this,"Aby przejść do dawkowania, " +
-                            "najpierw zaznacz lek.",Toast.LENGTH_SHORT).show();
-                }
+                DocumentReference docRef = db.collection("useremail@gmail.com").document("lekiWszystkie");
+                Map<String, Object> updates = new HashMap<>();
+                //PRZEROBIC NAZWY
+
+                Map<String, Object> deleteSong = new HashMap<>();
+                deleteSong.put("Lek2", FieldValue.delete());
+
+                FirebaseFirestore.getInstance()
+                        .collection("useremail@gmail.com")
+                        .document("lekiWszystkie")
+                        .update(deleteSong);
             }
         });
-        table.requestLayout();
-
     }
 
 
     public void btnAddDrug(View view) {
-
-        /*Intent intent = new Intent(getApplicationContext(), AddDrugToAll.class);
+        Intent intent = new Intent(getApplicationContext(), AddDrugToAll.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
-        finish();*/
-    }
-
-    public void btnRemoveDrug(View view) {
-        //plus dodać usunięcie z bazy po tym wyznaczonym wyżej rowId i odświeżenie bazy po kazdym usunięciu,
-        //żeby sie od nwoa te leki ladowały już bez tego usuniętego
-
+        finish();
     }
 
     public void returnClicked(View view) {
@@ -175,3 +192,4 @@ public class AllDrugs extends AppCompatActivity {
         finish();
     }
 }
+
