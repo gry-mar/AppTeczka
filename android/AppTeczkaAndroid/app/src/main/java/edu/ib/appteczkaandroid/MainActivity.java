8329fec59238715faac44295812aa9807477c152
Martyna Fojcik <254681@student.pwr.edu.pl>
2022-06-05 22:36:22 +0200

    frontend and internet access

--- android/AppTeczkaAndroid/app/src/main/java/edu/ib/appteczkaandroid/MainActivity.java
+++ android/AppTeczkaAndroid/app/src/main/java/edu/ib/appteczkaandroid/MainActivity.java
@@ -3,36 +3,76 @@ package edu.ib.appteczkaandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
+    private TextView tvNoInteret;
+
+    private Button btnGoNext;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mAuth = FirebaseAuth.getInstance();
+
+        btnGoNext = findViewById(R.id.button2);
+        tvNoInteret = findViewById(R.id.tvNoInternet);
+
+        if (haveNetwork()){
+            tvNoInteret.setText("Połączenie z internetem nawiązane.");
+            btnGoNext.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    FirebaseUser currentUser = mAuth.getCurrentUser();
+                    if(currentUser == null) {
+                        Intent intent = new Intent(MainActivity.this, Login.class);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(intent);
+                        finish();
+                    }else{
+                        Intent intent = new Intent(MainActivity.this, MenuAll.class);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(intent);
+                        finish();
+                    }
+                }
+            });
+        } else if (!haveNetwork()) {
+            tvNoInteret.setText("BRAK POŁĄCZENIA Z INTERNETEM.");
+            btnGoNext.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(MainActivity.this, "Do uruchomienia aplikacji wymagane jest połączenie z internetem.", Toast.LENGTH_LONG).show();
+
+                }
+            });
+        }
+
     }
 
-    public void btnGo(View view) {
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        if(currentUser == null) {
-            Intent intent = new Intent(this, Login.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            startActivity(intent);
-            finish();
-        }else{
-            Intent intent = new Intent(this, MenuAll.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            startActivity(intent);
-            finish();
+    private boolean haveNetwork(){
+        boolean have_WIFI= false;
+        boolean have_MobileData = false;
+        ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(CONNECTIVITY_SERVICE);
+        NetworkInfo[] networkInfos = connectivityManager.getAllNetworkInfo();
+        for(NetworkInfo info:networkInfos){
+            if (info.getTypeName().equalsIgnoreCase("WIFI"))if (info.isConnected())have_WIFI=true;
+            if (info.getTypeName().equalsIgnoreCase("MOBILE DATA"))if (info.isConnected())have_MobileData=true;
         }
+        return have_WIFI||have_MobileData;
     }
 
 }
